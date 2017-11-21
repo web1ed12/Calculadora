7203870c57f314a1bd43d2703f56daeb4562760a
web1ed12 <victorcampezgz@gmail.com>
2017-11-21 20:28:01 +0100

    estructura

--- calculadora/src/calculadora.java
+++ calculadora/src/calculadora.java
@@ -1,8 +1,24 @@
-
+import java.util.Scanner;
 public class calculadora {
-
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		double num1, num2, resultado;
+		int eleccion;
+		Scanner teclado = new Scanner(System.in);
+		System.out.println("Introduce primer numero");
+		num1 = teclado.nextDouble();
+		System.out.println("Introduce segundo numero");
+		num2 = teclado.nextDouble();
+		System.out.println("1(suma), 2(resta), 3(multiplicacion), 4(division)");
+		eleccion = teclado.nextInt();
+		switch (eleccion) {
+		case 3:
+			System.out.println("Producto: " + num1 * num2);
+			break;
+
+		default:
+			break;
+		}
 
 	}
 
